d40dc709357dafecff3cd1604077548435def4ce
matrix103 <82279887+matrix103@users.noreply.github.com>
2022-06-30 19:09:12 +0300

    Delete js directory

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-const burgerMenu = document.querySelector('.nav__burger');
-if(burgerMenu){
-   const menuBody = document.querySelector('.nav__items');
-   burgerMenu.addEventListener('click', function(e){
-      document.body.classList.toggle('_scrolLLock');
-      burgerMenu.classList.toggle('_activeBurger');
-      menuBody.classList.toggle('_activeBody');
-   })
-}
